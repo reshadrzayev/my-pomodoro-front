5f11a2f8cc88ced593fe471939371a8838cb8f25
reshad <reshadrzayev00@gmail.com>
2022-09-10 04:25:17 +0400

    added some temprorary btn for gh-pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import {BrowserRouter,Routes,Route,Link} from "react-router-dom";
 
 import App from './App';
 import Navbar from "./components/Navbar";
@@ -18,6 +18,7 @@ import Login from "./pages/auth/Login";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
+        {window.location.pathname === "/" && <Link to="/">Home</Link>}
         <Routes>
             <Route exact={true} path="/" element={<App/>}/>
             <Route path="/auth/register" element={<Register/>}/>
